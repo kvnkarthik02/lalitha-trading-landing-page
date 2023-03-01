88a32bd6be6e74b998d07c560b93dfc6a47cd46e
Karthik Venkatesh <kvenkate@tcd.ie>
2023-03-01 00:26:38 +0000

    init

---
+++ company-landing-page/src/components/Navbar.svelte
@@ -0,0 +1,44 @@
+<nav role="navigation">
+    <div class="companyName">
+        <a href="/">Sree Lalitha Trading Company</a>
+    </div>
+
+    <div class="navLinks" id="navLinks">
+        <a href="#about">About Us</a>
+        <a href="#products">Products</a>
+        <a href="#contact">Contact</a>
+    </div>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        background-color: #0362fc;
+    }
+
+    .companyName a {
+        color: #fcc603;
+    }
+
+    nav a {
+        color: #0362fc;
+		padding: 0 0.5rem;
+		font-weight: bold;
+	}
+
+    nav a:hover {
+		text-decoration: none;
+	}
+    nav div {
+		flex: 1;
+		padding: 1rem;
+	}
+
+    .navLinks {
+		text-align: right;
+		background-color: #fcc603;
+	}
+
+
+</style>
+
